95e46b72a2d531172a5f25539ac260939382867a
SaiAdithya3 <saiadithyakancharla@gmail.com>
2024-09-24 18:46:55 +0530

    okay

--- src/components/ThreeImages.jsx
+++ src/components/ThreeImages.jsx
@@ -1,52 +1,32 @@
-import React, { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
-import image from "../assets/image.png";
+import React from "react";
+import { motion } from "framer-motion";
 
 const ThreeImages = () => {
-  const containerRef = useRef(null);
-
-  const { scrollYProgress } = useScroll({
-    target: containerRef,
-    offset: ["start start", "end start"], 
-  });
-
-  const leftDivWidth = useTransform(scrollYProgress, [0, 1], ["50%", "1rem"]); 
-  const rightDivWidth = useTransform(scrollYProgress, [0, 1], ["50%", "1rem"]);
-
   return (
-    <div className="h-[200vh] relative ">
-      <div
-        ref={containerRef}
-        className="absolute top-0 left-0 w-full z-20 h-screen flex items-center justify-between"
+    <div className="w-full h-screen flex items-center justify-between">
+      {/* Left Image */}
+      <motion.div
+        className="relative h-full w-1/2 bg-red-200 bg-cover bg-center"
+        // style={{
+        //   backgroundImage: "url(https://newsroom.cdn.bugatti-media.com/0f204a25-28ba-482e-a2ab-630d3986e796/original)",
+        // }}
+        whileHover={{ width: "70%" }} // Expands to 70% on hover
       >
-        <motion.div
-          style={{ width: leftDivWidth }}
-          className="relative h-full flex bg-red-200 items-center justify-center
-          bg-[url(https://newsroom.cdn.bugatti-media.com/0f204a25-28ba-482e-a2ab-630d3986e796/original)]
-          bg-cover bg-center bg=
-          "
-          whileHover={{ width: "70%" }} 
-        >
-          <span className="absolute top-1/2 left-1/3 text-3xl text-white">Left</span>
-          {/* <img src="" alt="Image" className="h-full w-full object-cover" /> */}
-        </motion.div>
+        <img src="https://newsroom.cdn.bugatti-media.com/0f204a25-28ba-482e-a2ab-630d3986e796/original" alt="Image" className="w-full h-full object-cover "/>
+        {/* <span className="absolute top-1/2 left-1/3 text-3xl text-white">Left</span> */}
+      </motion.div>
 
-        {/* Right Div */}
-        <motion.div
-          style={{ width: rightDivWidth }}
-          className="relative h-full flex bg-blue-200 items-center justify-center
-          bg-[url(https://rossoautomobili.com/cdn/shop/articles/Ferrari_SF90_XX_-_Fiorano_4.jpg?v=1700067961)]
-          
-          bg-cover bg-center"
-          whileHover={{ width: "70%" }} 
-        >
-          {/* <img src="https://rossoautomobili.com/cdn/shop/articles/Ferrari_SF90_XX_-_Fiorano_4.jpg?v=1700067961" alt="Image" className="h-full w-full object-cover" /> */}
-          <span className="absolute top-1/2 left-1/2 text-3xl text-white">Right</span>
-        </motion.div>
-      </div>
-      <div className="sticky h-screen bg-green-200 top-0 left-0 w-full">
-        <img src={image} alt="Image" className="h-full w-full object-cover" />
-      </div>
+      {/* Right Image */}
+      <motion.div
+        className="relative h-full w-1/2 bg-blue-200 bg-cover bg-center"
+        // style={{
+        //   backgroundImage: "url(https://rossoautomobili.com/cdn/shop/articles/Ferrari_SF90_XX_-_Fiorano_4.jpg?v=1700067961)",
+        // }}
+        whileHover={{ width: "70%" }} // Expands to 70% on hover
+      >
+        <img src="https://rossoautomobili.com/cdn/shop/articles/Ferrari_SF90_XX_-_Fiorano_4.jpg?v=1700067961" alt="" className="w-full h-full object-cover" />
+        {/* <span className="absolute top-1/2 left-1/2 text-3xl text-white">Right</span> */}
+      </motion.div>
     </div>
   );
 };
